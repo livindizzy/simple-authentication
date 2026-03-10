20e370a23a0a099bf42781a9746a1c64a0de4880
Abdul Aziz Rizqi N <abdulazizrn14@gmail.com>
2026-03-10 11:55:02 +0700

    init project

---
+++ simple-authetication/src/main/java/com/authentication/simple_authetication/SimpleAutheticationApplication.java
@@ -0,0 +1,13 @@
+package com.authentication.simple_authetication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAutheticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAutheticationApplication.class, args);
+	}
+
+}
---
+++ simple-authetication/src/test/java/com/authentication/simple_authetication/SimpleAutheticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentication.simple_authetication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAutheticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
